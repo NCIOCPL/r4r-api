8f657cd576bcaa17f0cd21e57497db389e0b8bd9
Blair Learn <blairl-nih@users.noreply.github.com>
2022-10-28 11:30:10 -0400

    Reduce to the simplest case

---
+++ test/NCI.OCPL.Api.ResourcesForResearchers.Tests/Tests/Simple.test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+using Xunit;
+
+
+
+namespace NCI.OCPL.Api.ResourcesForResearchers.Tests.Services
+{
+    public class Simple_Tests
+    {
+
+        [Fact]
+        public void ReallySimple()
+        {
+            Assert.Equal(1,1);
+        }
+
+
+    }
+}
