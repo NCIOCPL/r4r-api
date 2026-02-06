2fe62d0841c382a388bfb33edb847fae397ba0e0
Blair Learn <blairlearn@users.noreply.github.com>
2026-02-06 18:11:18 -0500

    Remove unused using statements.

--- src/NCI.OCPL.Api.ResourcesForResearchers/Controllers/ResourceController.cs
+++ src/NCI.OCPL.Api.ResourcesForResearchers/Controllers/ResourceController.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 using NCI.OCPL.Api.Common;
 
--- src/NCI.OCPL.Api.ResourcesForResearchers/Controllers/ResourcesController.cs
+++ src/NCI.OCPL.Api.ResourcesForResearchers/Controllers/ResourcesController.cs
@@ -1,16 +1,13 @@
 ﻿using System;
-using System.Web;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Newtonsoft.Json;
 
 using NCI.OCPL.Api.Common;
 
